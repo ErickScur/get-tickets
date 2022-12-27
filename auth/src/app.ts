acc9b8dabea7ce56037e9631bc501fd5d1ded9af
Erick Scur <erickscur@gmail.com>
2022-12-27 11:18:24 -0300

    feat: mongodb connection

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -1,11 +1,14 @@
+import mongoose from 'mongoose'
 import express from 'express'
 import 'express-async-errors'
 import { json } from 'body-parser'
-import { currentUserRouter } from './routes/current-user'
-import { signInRouter } from './routes/signin'
-import { signOutRouter } from './routes/signout'
-import { signUpRouter } from './routes/signup'
 import { errorHandler } from './middlewares/error-handler'
+import {
+  currentUserRouter,
+  signInRouter,
+  signOutRouter,
+  signUpRouter,
+} from './routes'
 
 const app = express()
 app.use(json())
@@ -17,6 +20,16 @@ app.use(signUpRouter)
 
 app.use(errorHandler)
 
-app.listen(3000, () => {
-  console.log('Listening on 3000')
-})
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth')
+    console.log('Connected to MongoDB')
+    app.listen(3000, async () => {
+      console.log('Listening on 3000')
+    })
+  } catch (error) {
+    throw error
+  }
+}
+
+start()

b1c54b9024b78e3a5a5f51e1f38099d52776dda6
Erick Scur <erickscur@gmail.com>
2023-01-02 14:47:33 -0300

    feat: add cookie-session

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -3,6 +3,8 @@ import express from 'express'
 import 'express-async-errors'
 import { json } from 'body-parser'
 import { errorHandler } from './middlewares/error-handler'
+import cookieSession from 'cookie-session'
+
 import {
   currentUserRouter,
   signInRouter,
@@ -11,7 +13,15 @@ import {
 } from './routes'
 
 const app = express()
+app.set('trust proxy', true)
+
 app.use(json())
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  }),
+)
 
 app.use(currentUserRouter)
 app.use(signInRouter)
